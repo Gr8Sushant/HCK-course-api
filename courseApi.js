d8da3a6445e8471fa21030ddb3bd307e758eb3c5
Gr8Sushant <spokharel95@gmail.com>
2021-08-31 20:34:37 +0545

    level 4 and 5 courses updated

--- courseApi.js
+++ courseApi.js
@@ -7,10 +7,24 @@ app.use(express.json());
 
 const courses = [
     {id: 1, name: 'Complex System', code: '6CS014', level: '6' },
-    {id: 2, name: 'High Performance Computing', code: '6CS014', level: '6' },
-    {id: 3, name: 'Project and Professionalism', code: '6CS014', level: '6' },
-    {id: 4, name: 'Artificial Intelligence and Machine Learning', code: '6CS014', level: '6' },
-    {id: 5, name: 'Big Data', code: '6CS014', level: '6' }
+    {id: 2, name: 'High Performance Computing', code: '6CS005', level: '6' },
+    {id: 3, name: 'Project and Professionalism', code: '6CS007', level: '6' },
+    {id: 4, name: 'Artificial Intelligence and Machine Learning', code: '6CS012', level: '6' },
+    {id: 5, name: 'Big Data', code: '6CS030', level: '6' },
+
+    {id: 6, name: 'Concepts and Technologies of AI', code: '5CS037', level: '5' },
+    {id: 7, name: 'Object-Oriented Design and Programming', code: '5CS019', level: '5' },
+    {id: 8, name: 'Numerical Methods and Concurrency', code: '5CS021', level: '5' },
+    {id: 9, name: 'Distributed and Cloud System Programming', code: '5CS022', level: '5' },
+    {id: 10, name: 'Collaborative Development', code: '5CS024', level: '5' },
+    {id: 11, name: 'Human Computer Interaction', code: '5CS020', level: '5' },
+
+    {id: 12, name: 'Academic Skills and Team Based Learning', code: '4CI018', level: '4' },
+    {id: 13, name: 'Introductory Programming and Problem Solving', code: '4CS001', level: '4' },
+    {id: 14, name: 'Fundamentals of Computing', code: '4CS015', level: '4' },
+    {id: 15, name: 'Embedded System Programming', code: '4CS014', level: '4' },
+    {id: 14, name: 'Internet Software Architecture', code: '4CS017', level: '4' },
+    {id: 15, name: 'Computational Mathematics', code: '4MM013', level: '4' }
 ]
 
 app.get('/', (req, res) => {
